5ba179b957d1e570a58dac9869f6108fe2e2440b
stefanovav <81904507+stefanovav@users.noreply.github.com>
2025-05-01 17:42:50 +0300

    Update Python-Streamlit.py

--- Python-Streamlit.py
+++ Python-Streamlit.py
@@ -45,7 +45,7 @@ import time
 HOST = "https://app.speckle.systems"
 rhino_compute_url = "http://localhost:6500/io"
 compute_rhino3d.Util.url = rhino_compute_url
-GH_FILE_PATH = r"https://raw.githubusercontent.com/stefanovav/StreamliApp/main/22.Video/250218_GHScript_Karamba3d_STB%20Platte-1.gh"
+GH_FILE_PATH = "https://raw.githubusercontent.com/stefanovav/StreamliApp/main/22.Video/250218_GHScript_Karamba3d_STB%20Platte-1.gh"
 WEBHOOK_URL = "https://speckle-webhook-nx44ryeaaq-nw.a.run.app/latest"
 
 
@@ -431,8 +431,7 @@ def main():
     client, account = authenticate_with_speckle()
     if not client or not account:  # Check if authentication failed
         return
-    speckle_token = account.token
-
+    
 
 
     # 🟢 Step 2: Get Project URL and Extract ID
