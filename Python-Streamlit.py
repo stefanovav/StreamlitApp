a2358d0a165a545e7fdf52acf53fe146f0782dee
stefanovav <81904507+stefanovav@users.noreply.github.com>
2025-05-28 21:45:13 +0300

    Update Python-Streamlit.py

--- Python-Streamlit.py
+++ Python-Streamlit.py
@@ -43,7 +43,7 @@ import time
 
 # Speckle server configuration
 HOST = "https://app.speckle.systems"
-rhino_compute_url = "https://savannah-done-newsletters-accordance.trycloudflare.com/io"
+rhino_compute_url = "http://localhost:8081"
 compute_rhino3d.Util.url = rhino_compute_url
 GH_FILE_PATH = r"https://raw.githubusercontent.com/stefanovav/StreamlitApp/main/GHScript_Karamba3d_2d%20Structures_Shell_22Video.gh"
 
