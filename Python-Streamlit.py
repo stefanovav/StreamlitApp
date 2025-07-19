6cd3154a2495832a5c4c4a37e842a95f76f8dc64
stefanovav <81904507+stefanovav@users.noreply.github.com>
2025-07-19 17:10:57 +0300

    Update Python-Streamlit.py

--- Python-Streamlit.py
+++ Python-Streamlit.py
@@ -354,7 +354,6 @@ def main():
 
                 # 🟢 Step 8: Extract and Display Utilization Value
                 transport = ServerTransport(client=client, stream_id=project_id, account=account)
-                st.write("Referenced object ID:", version.referencedObject)
 
                 res = operations.receive(version.referencedObject, transport)
 
